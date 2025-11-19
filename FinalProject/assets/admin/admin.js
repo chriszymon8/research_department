408f46177ae2bece3eefa04bb71e0f003095aca9
Zymon88 <chriszymonbiado@gmail.com>
2025-11-19 15:37:31 +0800

    finalleeee

--- FinalProject/assets/admin/admin.js
+++ FinalProject/assets/admin/admin.js
@@ -101,8 +101,8 @@ async function renderProducts(products = []) {
 
     const img = document.createElement("img");
     img.className = "w-16 h-16 object-contain";
+    img.src = p.img || "watches/placeholder.avif"; 
     tdImg.appendChild(img);
-    tr.appendChild(tdImg);
 
     const tdActions = document.createElement("td");
     tdActions.className = "p-2 flex gap-2";
